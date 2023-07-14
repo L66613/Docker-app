65d6f03665b637076ccf3f673d4fb854dca523b9
Elena Bazhenova <lena130596@mail.ru>
2023-07-14 15:56:47 +0900

    first

---
+++ simple-docker-application/src/main/java/ru/itgirl/simpledockerapplication/SimpleDockerApplication.java
@@ -0,0 +1,13 @@
+package ru.itgirl.simpledockerapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDockerApplication.class, args);
+	}
+
+}
---
+++ simple-docker-application/src/test/java/ru/itgirl/simpledockerapplication/SimpleDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itgirl.simpledockerapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
